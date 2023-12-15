2048b134d69e162a8ccbdd0db97fc79617e040b6
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-12-15 20:26:50 +0800

    [Feat][Lex] Added markdown preview plugin

--- lua/laplace/packer.lua
+++ lua/laplace/packer.lua
@@ -99,6 +99,13 @@ return require("packer").startup(function(use)
             vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
         end
     }
+    
+    use { 
+        "iamcco/markdown-preview.nvim",
+        run = "cd app && npm install",
+        setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+        ft = { "markdown" }
+    }
 
     use {
         "nvim-treesitter/nvim-treesitter",

de23f3120dbf0779d6541d07257d2b32988ef1df
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-12-29 00:50:29 +0800

    [Fix][Lex] Removed the autostart of markdown preview whenever a markdown file is opened

--- lua/laplace/lazy.lua
+++ lua/laplace/lazy.lua
@@ -320,7 +320,6 @@ require("lazy").setup({
         build = "cd app && yarn install",
         init = function()
             vim.g.mkdp_combine_preview = 1
-            vim.g.mkdp_auto_start = 1
             vim.g.mkdp_auto_close = 0
 
             vim.g.mkdp_filetypes = { "markdown" }

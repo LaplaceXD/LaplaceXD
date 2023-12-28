836eadc37737bb82e2e571e7b022ed2c91faefe2
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-12-28 23:43:11 +0800

    [Fix][Lex] Fixed changing tab names in windows terminal

--- lua/laplace/lazy.lua
+++ lua/laplace/lazy.lua
@@ -130,7 +130,7 @@ require("lazy").setup({
         },
         config = function()
             local conform = require("conform")
-            
+
             conform.setup({
                 formatters_by_ft = {
                     lua = { "stylua" },

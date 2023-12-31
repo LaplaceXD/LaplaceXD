f7eb2a022303ec3a4e5ae38d3f53d125e2c4d28e
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-12-31 20:04:14 +0800

    [Fix][Lex] Fixed LSP semantic token highlighting not working as well with material, and fixed issue with rainbow-delimiters with tsx

--- lua/laplace/plugins/ui.lua
+++ lua/laplace/plugins/ui.lua
@@ -13,8 +13,12 @@ return {
 	{
 		"HiPhish/rainbow-delimiters.nvim",
 		event = "VeryLazy",
-		config = function()
-			vim.g.rainbow_delimiters = { highlight = { "Yellow", "Purple", "Blue" } }
+		opts = {
+			highlight = { "MaterialYellow", "MaterialPurple", "MaterialBlue" },
+			query = { tsx = "rainbow-parens" },
+		},
+		config = function(_, opts)
+			require("rainbow-delimiters.setup").setup(opts)
 		end,
 	},
 
@@ -29,7 +33,7 @@ return {
 			},
 			scope = {
 				enabled = true,
-				highlight = { "Yellow", "Purple", "Blue" },
+				highlight = { "MaterialYellow", "MaterialPurple", "MaterialBlue" },
 				show_exact_scope = true,
 				show_start = false,
 				show_end = false,
@@ -84,11 +88,6 @@ return {
 				"trouble",
 			},
 			custom_colors = function(colors)
-				-- Bracket Colors
-				vim.api.nvim_set_hl(0, "Yellow", { fg = colors.main.yellow })
-				vim.api.nvim_set_hl(0, "Purple", { fg = colors.main.purple })
-				vim.api.nvim_set_hl(0, "Blue", { fg = colors.main.blue })
-
 				colors.main.black = "" -- make statusline background transparent
 				colors.main.darkpurple = colors.main.paleblue -- darkpurple is too harsh for lsp autosuggestions
 				colors.editor.accent = colors.main.darkpurple
@@ -101,7 +100,65 @@ return {
 		config = function(_, opts)
 			require("material").setup(opts)
 			vim.g.material_style = "palenight"
-			vim.cmd([[colorscheme material]])
+			vim.cmd.colorscheme("material")
+
+			local colors = require("material.colors")
+			-- colors for brackets
+			vim.api.nvim_set_hl(0, "MaterialYellow", { fg = colors.main.yellow })
+			vim.api.nvim_set_hl(0, "MaterialPurple", { fg = colors.main.purple })
+			vim.api.nvim_set_hl(0, "MaterialBlue", { fg = colors.main.blue })
+
+			-- color overrides for LSP
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+					if client.name == "clangd" then
+						vim.api.nvim_set_hl(0, "@lsp.type.class.c", { fg = colors.main.yellow })
+						vim.api.nvim_set_hl(0, "@lsp.type.macro.c", { fg = colors.main.blue })
+					elseif client.name == "pyright" then
+						vim.api.nvim_set_hl(0, "@type.python", { fg = colors.main.yellow })
+					elseif client.name == "tsserver" then
+						local links = {
+							[colors.main.yellow] = {
+								-- BRUHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+								-- I pray for someone builds a material colorscheme
+								-- that is more 1:1 with vscode or I'm gonna waste
+								-- my entire life building one
+								"@lsp.typemod.enumMember.readonly.typescriptreact",
+								"@lsp.typemod.type.defaultLibrary.typescriptreact",
+								"@lsp.typemod.class.defaultLibrary.typescriptreact",
+								"@lsp.typemod.interface.defaultLibrary.typescriptreact",
+								"@lsp.typemod.interface.declaration.typescriptreact",
+								"@lsp.typemod.type.declaration.typescriptreact",
+								"@lsp.type.type.typescriptreact",
+								"@lsp.type.interface.typescriptreact",
+
+								"@lsp.typemod.enumMember.readonly.typescript",
+								"@lsp.typemod.interface.declaration.typescript",
+								"@lsp.typemod.type.declaration.typescript",
+								"@lsp.type.type.typescript",
+								"@lsp.type.interface.typescript",
+
+								"@constructor.tsx",
+								"@type.typescript",
+								"@type.tsx",
+							},
+							[colors.editor.fg] = "@constant",
+						}
+
+						for color, hl in pairs(links) do
+							if type(hl) == "table" then
+								for _, h in ipairs(hl) do
+									vim.api.nvim_set_hl(0, h, { fg = color })
+								end
+							else
+								vim.api.nvim_set_hl(0, hl, { fg = color })
+							end
+						end
+					end
+				end,
+			})
 		end,
 	},
 }

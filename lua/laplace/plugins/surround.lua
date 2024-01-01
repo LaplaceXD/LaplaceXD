62d55d35a349bb15fe2b640a88bb48ea280bda46
Jonh Alexis Buot <ja.buotph@gmail.com>
2024-01-01 09:05:42 +0800

    [Fix][Lex] Added missing keys to surround

--- lua/laplace/plugins/surround.lua
+++ lua/laplace/plugins/surround.lua
@@ -3,7 +3,10 @@ return {
 		"kylechui/nvim-surround",
 		keys = {
 			{ "ys", desc = "Insert surrounding character command prefix.", mode = "n" },
+			{ "yss", desc = "Insert surrounding character on the current line command prefix.", mode = "n" },
+			{ "S", desc = "Insert surrounding character on the selection command prefix.", mode = "n" },
 			{ "cs", desc = "Change surrounding character command prefix.", mode = "n" },
+			{ "cS", desc = "Change surrounding character and put changes on new lines command prefix.", mode = "n" },
 			{ "ds", desc = "Delete surrounding character command prefix.", mode = "n" },
 		},
 		opts = {},

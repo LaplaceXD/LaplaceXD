a5bb70a527e78a10cc3427671dad1fae45c23d81
Jonh Alexis Buot <ja.buotph@gmail.com>
2024-01-18 00:21:00 +0800

    [Feat][Lex] Removed lazy-lock.json and updated yank highlight timeout

--- lua/laplace/term.lua
+++ lua/laplace/term.lua
@@ -58,7 +58,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Hightlight selection on yank",
 	pattern = "*",
 	callback = function()
-		vim.highlight.on_yank({ higroup = "Visual", timeout = 500 })
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 300 })
 	end,
 })
 

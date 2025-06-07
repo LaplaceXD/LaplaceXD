933d879216f8f70bc651ce54020a8d3b8a50c52a
Jonh Alexis Buot <ja.buotph@gmail.com>
2025-06-07 22:54:12 +0800

    fix: highlighted entire lines with diagnostic color

--- lua/laplace/term.lua
+++ lua/laplace/term.lua
@@ -62,7 +62,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- Diagnostic Configs
 vim.diagnostic.config({
 	update_in_insert = true,
 	severity_sort = true,
@@ -74,12 +73,6 @@ vim.diagnostic.config({
 			[vim.diagnostic.severity.WARN] = "",
 			[vim.diagnostic.severity.HINT] = "⚑",
 			[vim.diagnostic.severity.INFO] = "»",
-		},
-		linehl = {
-			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
 		},
 		numhl = {
 			[vim.diagnostic.severity.ERROR] = "",
